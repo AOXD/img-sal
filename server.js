dd87601b3f89d763e21c0bc4542c294a0628f427
AOXD <aoxdcode@gmail.com>
2016-12-30 14:17:29 +0100

    add routes to server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// ...
+const routes = require('app/routes/index.js');
+// 2.
 require('dotenv').config({
   silent: true
 });
@@ -25,7 +26,8 @@ mongoose.connect(mongoURI);
 app.set('views', path.join(_dirname, + 'views'));
 app.set('view engine', 'pug');
 
-// ...
+routes(app);
+// 2.
 
 app.listen(port, function(req, res) {
   console.log('App listening on port ' + port);
